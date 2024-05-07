5ca4a9e7175562d41047ef1e19420e97d2421026
KeyEno2k2 <igor.jurewicz.wej@gmail.com>
2024-05-07 11:50:28 +0200

    Second commit

--- src/code/Gameplay.ts
+++ src/code/Gameplay.ts
@@ -26,6 +26,7 @@ import {
  import { StaticObject } from "./StaticObjects"
  import { DEG2RAD, RAD2DEG, randFloat, randInt } from "three/src/math/MathUtils"
  import { SkeletonData, SkeletonMesh } from "playable-dev/spine-lib"
+ import { ArrowTrace } from "./ArrowTrace"
 
  const THROWS: number = 3;
  const RESTART_TIME_MILISECONDS: number = 2200;
@@ -57,5 +58,61 @@ import {
 
     sr: ScreenResizer;
     hitAnimationPlayed: boolean = false;
+    cameraActive: boolean = false;
+    updateTargetColliderBlockade: boolean = false;
+    arrowTrace?: ArrowTrace;
+    cameraDistance: number = 3.4;
+    music?: AWAudio;
+    interacted: boolean = false;
+    startCameraDistance: number = 0;
+    layerChanged1: boolean = false;
+    layerChanged2: boolean = false;
+    layerchanged3: boolean = false;
+    startAnimationScale: number = 0;
+
+    firstAnimationTruning?: Animator;
+    secondAnimationTurning?: Animator;
+    thirdAnimationTurning?: Animator;
+
+    fistAnimationStarted?: boolean;
+    secondAnimationStarted?: boolean;
+    thirdAnimationStarted?: boolean;
+
+    firstAnimationCurrentScale: number = 0;
+    secondAnimationCurrentScale: number = 0;
+    thirdAnimationCurrentScale: number = 0;
+
+    playMusicAgainBlockade: boolean = false;
+    hits: number = 0;
+
+    constructor(){
+        this.sr = ScreenResizer.resizer;
+        Engine.controlEvents.addListener(this);
+        if (!this.playMusicAgainBlockade){
+            this.playMusicAgainBlockade = true;
+            this.music = playSndLoop("music", 0.2);
+        }
+
+        if (Game.sessionCounter > 1){
+            return;
+        }
+
+        this.arrow = StaticObject.arrow;
+        this.arrow.renderOrder = 1;
+
+        this.arrowStartPosition = this.arrow.position.clone();
+
+        this.cameraStartPosition = Engine.camera.position.clone();
+
+        this.lightStartPosition = Game.game.scene.directionalLight!.position.clone();
+
+        this.arrowTrace = new ArrowTrace();
+
+        this.cameraDistance = Engine.camera.position.z - this.arrow.position.z;
+        this.startCameraDistance = this.cameraDistance;
+
+    }
+
+
     
  }
